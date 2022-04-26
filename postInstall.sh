31ccfa900e6e24b24f57c6d7600dbe2c8076c4a1
SAJO Co <87914407+sajo-dev@users.noreply.github.com>
2022-04-26 14:37:26 -0300

    Update postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -1,3 +1,9 @@
-mkdir -p /var/log/suricata
-chown -R root:suri /var/log/suricata
-chmod -R 775 /var/log/suricata
+sudo suricata-update
+sudo useradd suri
+sudo usermod -a -G suri suri
+sudo chgrp -R suri /etc/suricata
+sudo chmod -R g+r /etc/suricata
+sudo mkdir -p /var/log/suricata
+sudo chown -R root:suri /var/log/suricata
+sudo chmod -R 775 /var/log/suricata
+sudo chmod -R g+rw /var/lib/suricata
